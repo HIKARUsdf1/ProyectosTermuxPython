40704fa9e9160c95009d974091606ae99c986819
HIKARUsdf1 <marcosmark9100@gmail.com>
2026-07-30 19:18:26 -0500

    Primeros archivos desde Ternux a git

---
+++ cal.py
@@ -0,0 +1,5 @@
+num1 = int(input("Escribe un numero:   "))
+num2 = int(input("Ingresa segundo numero:   "))
+resultado = num1 + num2
+print("La suma es:   ", resultado)
+
---
+++ hola.py
@@ -0,0 +1,1 @@
+print ("Hola desde nano y python !!")
